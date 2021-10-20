2072af61c310e98efcf5d63d4aefede09b294b9e
Alicja Malkiewicz <alicjamalkiewicz1@gmail.com>
2021-10-20 19:51:34 +0200

    adding checkHomePageUrl method

--- ShopTests.java
+++ ShopTests.java
@@ -8,4 +8,11 @@ public class ShopTests extends BaseTest{
         driver.get("http://sampleshop.inqa.pl/");
         Assert.assertEquals(driver.getTitle(), "Automation Sample Shop");
     }
+
+    @Test
+    public void checkHomePageUrl(){
+        driver.get("http://sampleshop.inqa.pl/");
+        Assert.assertEquals(driver.getCurrentUrl(), "http://sampleshop.inqa.pl/");
+
+    }
 }
