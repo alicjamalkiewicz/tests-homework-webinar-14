2ccd76eaa8af8e9ad337742c15014aad01871270
Alicja Malkiewicz <alicjamalkiewicz1@gmail.com>
2021-10-27 18:48:48 +0200

    poprawione WebElements + lista sprawdzajaca liczbe linków

--- ZadanieDomowe7.java
+++ ZadanieDomowe7.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,22 +18,27 @@ public class ZadanieDomowe7 extends BaseTest{
         WebDriverWait wait = new WebDriverWait(driver, 1);
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("lnXdpd")));
 
-        WebElement searchFiled = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
-        searchFiled.sendKeys("JAVA");
-        searchFiled.click();
+//        WebElement searchFiled = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+//        searchFiled.sendKeys("JAVA");
+//        searchFiled.click();
 
-        WebElement searchButton = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[5]/center/input[1]"));       //(By.className("CcAdNb"));
-        searchButton.click();
+        driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
 
-        List<WebElement> howManyLinks = driver.findElements(By.cssSelector(".LC20lb.DKV0Md"));
-        System.out.println(howManyLinks.size());
+//        WebElement searchButton = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[5]/center/input[1]"));       //(By.className("CcAdNb"));
+//        searchButton.click();
 
-        List<WebElement> howManyLinks2 = driver.findElements(By.cssSelector(".l"));
-        System.out.println(howManyLinks2.size());
+//        List<WebElement> howManyLinks = driver.findElements(By.cssSelector(".LC20lb.DKV0Md"));
+//        System.out.println(howManyLinks.size());
+//
+//        List<WebElement> howManyLinks2 = driver.findElements(By.cssSelector(".l"));
+//        System.out.println(howManyLinks2.size());
+//
+//        int linksOnThePage = howManyLinks.size() + howManyLinks2.size();
 
-        int linksOnThePage = howManyLinks.size() + howManyLinks2.size();
+//        System.out.println("On the 1st page, there are " + linksOnThePage + " links in total.");
 
-        System.out.println("On the 1st page, there are " + linksOnThePage + " links in total.");
+        List<WebElement> results = driver.findElements(By.className("g"));
+        System.out.println(results.size());
 
     }
 }
