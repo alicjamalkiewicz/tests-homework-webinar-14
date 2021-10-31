1690788baedf0c84327e2a5ce9e8b2dbc825e8f7
Alicja Malkiewicz <alicjamalkiewicz1@gmail.com>
2021-10-31 08:36:54 +0100

    ZadanieDomowe6 poprawione

--- ZadanieDomowe8.java
+++ ZadanieDomowe8.java
@@ -1,8 +1,15 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class ZadanieDomowe8 extends BaseTest{
 
@@ -12,17 +19,42 @@ public class ZadanieDomowe8 extends BaseTest{
 
         WebElement acceptCokies = driver.findElement(By.xpath("//*[@id=\"gp-cookie-agreements\"]/div/div/div[1]/div[3]/button"));
         acceptCokies.click();
-//        WebDriverWait wait = new WebDriverWait(driver, 10);
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("recommendations--15VK5")));
+        WebDriverWait wait = new WebDriverWait(driver, 10);
 
-        WebElement searchJob = driver.findElement(By.xpath("//*[@id=\"kw-1634898959708\"]"));
-        searchJob.sendKeys("tester");
+//        WebElement descriptionField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/main/div[1]/div/div/div[3]/form/div[1]/div[2]/div/div/input")));
+//        descriptionField.sendKeys("Tester");
+//
+//        WebElement cityField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/main/div[2]/div/div/form/div[1]/div[3]/div/div/input")));
+//        cityField.sendKeys("Warszawa");
 
-//        WebElement searchCity = driver.findElement(By.xpath("//*[@id=\"wp-1634898856687\"]"));
-//        searchCity.sendKeys("Warszawa");
+        WebElement descriptionField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[text()='stanowisko, firma, słowo kluczowe']/../input")));
+        descriptionField.sendKeys("tester");
 
-//        WebElement submitButton = driver.findElement(By.className("slider--1Afpe"));
-//        submitButton.click();
+        WebElement cityField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[text()='miejscowość, województwo lub kraj']/../input")));
+        cityField.sendKeys("Warszawa");
+        cityField.sendKeys(Keys.ENTER);
+
+        WebElement submitButton = driver.findElement(By.className("slider--1Afpe"));
+        submitButton.click();
+
+        WebElement understoodButton = driver.findElement(By.className("popover__button"));
+        understoodButton.click();
+
+        List<WebElement> jobName = driver.findElements(By.className("offer-details__title-link"));
+        List<WebElement> companyName = driver.findElements(By.className("offer-details__title-link"));
+
+        //for(String s = 0; s <)
+
+
+
+
+
+
+
+
+
+        //offer-details__title-link
+
 
     }
 }
