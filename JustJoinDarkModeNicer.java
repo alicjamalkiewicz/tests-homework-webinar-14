59064adeb4cb461cb572e662027b03adafb3516a
Alicja Malkiewicz <alicjamalkiewicz1@gmail.com>
2021-11-01 09:48:47 +0100

    zadanie 3* 'nicer' version but not working

--- JustJoinDarkModeNicer.java
+++ JustJoinDarkModeNicer.java
@@ -27,11 +27,6 @@ public class JustJoinDarkModeNicer extends BaseTest {
 
         Assert.assertEquals(lightModeButtonsColorHex, "#99a1ab");
 
-//        WebElement lightModeAllOffersField = driver.findElement(By.xpath("//a[@class='css-116ia3n']//div[@class='css-9ygpzv']"));
-//        String lightModeAllOffersFieldColor = lightModeAllOffersField.getCssValue("background-color");
-//        String lightModeAllOffersFieldColorRGB = Color.fromString(lightModeAllOffersFieldColor).asRgb();
-//
-//        Assert.assertEquals(lightModeAllOffersFieldColorRGB, "rgb(33, 33, 33)");
     }
 
 
@@ -41,24 +36,15 @@ public class JustJoinDarkModeNicer extends BaseTest {
         WebElement colorModeToggle = driver.findElement(By.cssSelector("input[type='checkbox']"));
         colorModeToggle.click();
 
-        //WebElement background = driver.findElement(By.xpath("/html/body"));
         String darkModeColor = background.getCssValue("background-color");
         String darkModeColorRGB = Color.fromString(darkModeColor).asRgb();
 
         Assert.assertEquals(darkModeColorRGB, "rgb(33, 33, 33)");
 
-        //WebElement colorModeButton = driver.findElement(By.xpath("//span[normalize-space()='Brand Stories']"));
         String darkModeButtonsColor = colorModeButton.getCssValue("color");
         String darkModeButtonsColorRGBA = Color.fromString(darkModeButtonsColor).asRgba();
 
         Assert.assertEquals(darkModeButtonsColorRGBA, "rgba(255, 255, 255, 0.8)");
-//
-//        WebElement darkModeAllOffersField = driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[1]/div/div[1]/div[1]/a[2]/div"));
-//        String darkModeAllOffersFieldColor = darkModeAllOffersField.getCssValue("color");
-//        String darkModeAllOffersFieldColorRGB = Color.fromString(darkModeAllOffersFieldColor).asRgb();
-//
-//        Assert.assertEquals(darkModeAllOffersFieldColorRGB, "rgb(33, 33, 33)");
-
 
     }
 }
